ce813b452837af58e22cf75fb67df6b685f318d4
GSWilliam276 <wg00237@gmail.com>
2026-08-09 22:19:12 -0600

    Agregada bienvenida con arte ASCII del quetzal, colores ANSI y configuracion del jar ejecutable

--- src/main/java/com/promtzal/app/Promptzal.java
+++ src/main/java/com/promtzal/app/Promptzal.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Scanner;
+import com.promptzal.vista.Colores;
 /**
  *
  * @author eduar
@@ -15,8 +16,10 @@ import java.util.Scanner;
 public class Promptzal {
 
     public static void main(String[] args) {
+        mostrarBienvenida();
+
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Ingrese la ruta del archivo .pz a analizar:");
+        System.out.println(Colores.AZUL + "Ingrese la ruta del archivo .pz a analizar:" + Colores.RESET);
         String ruta = scanner.nextLine();
 
         try {
@@ -24,16 +27,33 @@ public class Promptzal {
             AnalizadorLexico analizador = new AnalizadorLexico(contenido);
             analizador.analizar();
             analizador.mostrarTokensEnConsola();
+
             GeneradorReporte generador = new GeneradorReporte();
             generador.generarReporteTokens(analizador.getListaTokens(), "reporte_tokens.html");
             generador.generarReporteErrores(analizador.getListaErrores(), "reporte_errores.html");
 
-            System.out.println("\nReportes HTML generados: reporte_tokens.html y reporte_errores.html");
-            
-            System.out.println("Analisis completado.");
-            
+            System.out.println(Colores.VERDE + "\nReportes HTML generados: reporte_tokens.html y reporte_errores.html" + Colores.RESET);
+
         } catch (IOException ex) {
-            System.out.println("Error: no se pudo leer el archivo. Verifica la ruta.");
+            System.out.println(Colores.ROJO + "Error: no se pudo leer el archivo. Verifica la ruta." + Colores.RESET);
         }
     }
+    
+        private static void mostrarBienvenida(){
+            System.out.println(Colores.NARANJA + Colores.NEGRITA);
+            System.out.println("    _");
+            System.out.println("  ~(o)>");
+            System.out.println("  / ) \\        ===================");
+            System.out.println(" ( (   )            PROMPTZAL");
+            System.out.println("  \\ \\_/        ===================");          
+            System.out.println("   ) )");
+            System.out.println("  ( (");
+            System.out.println("   ) )");
+            System.out.println("  ( (");
+            System.out.println("   \\ \\");
+            System.out.println("    \\ \\");
+            System.out.println("     `~'");
+            System.out.println(Colores.RESET);
+            System.out.println(Colores.AZUL + "Bienvenid@ a PromptZal - Analizador Lexico" + Colores.RESET);
+        }            
 }
